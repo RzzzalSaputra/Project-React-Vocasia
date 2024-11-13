3d6c44a66e68b480634d854cfa8e9e9737aff64f
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-11-13 23:54:23 +0700

    "Update/Remove App"

--- VocaTask/src/main.jsx
+++ VocaTask/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './config/routes'
+import { DummyData } from './config/Dummydata'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <DummyData>
+        <RouterProvider router={routes} />
+      </DummyData>
   </StrictMode>,
 )
