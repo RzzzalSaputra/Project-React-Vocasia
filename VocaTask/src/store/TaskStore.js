582d7081431bb26bc86a305d2640021282623e18
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-11-19 03:07:22 +0700

    "refactor: add persist to userStore and clean up task store"

--- VocaTask/src/store/TaskStore.js
+++ VocaTask/src/store/TaskStore.js
@@ -1,43 +1,53 @@
 import { create } from 'zustand'
 import { axiosInstance as api } from '../config/axiosInstance'
 
-const taskStore = create((set, get)=>({
-    task: [],
-    createTask: async (title)=>{
-        try {
-            const data = await api.post('/api/tasks', {title})
-            get().getTask()
-            return data
-        } catch (error) {
-            console.error(error)
+const taskStore = create(
+    (set, get)=>({
+        task: [],
+
+        // Create Task
+        createTask: async (title)=>{
+            try {
+                const data = await api.post('/api/tasks', {title})
+                get().getTask()
+                return data
+            } catch (error) {
+                console.error(error)
+            }
+        },
+
+        // Get Task
+        getTask: async ()=>{
+            try {
+                const res = await api.get('/api/tasks')
+                set({task: res.data})
+            } catch (error) {
+                console.error(error)
+            }
+        },
+
+        // Mark Done Task By Id
+        MarkDoneTaskById: async (_id)=>{
+            try {
+                const res = await api.patch(`/api/tasks/${_id}/done`)
+                get().getTask()
+                return res
+            } catch (error) {
+                console.error(error)
+            }
+        },
+
+        // Delete Task
+        deleteTask: async (_id)=>{
+            try {
+                const res = await api.delete(`/api/tasks/${_id}`)
+                get().getTask()
+                return res
+            } catch (error) {
+                console.error(error)
+            }
         }
-    },
-    getTask: async ()=>{
-        try {
-            const res = await api.get('/api/tasks')
-            set({task: res.data})
-        } catch (error) {
-            console.error(error)
-        }
-    },
-    MarkDoneTaskById: async (_id)=>{
-        try {
-            const res = await api.patch(`/api/tasks/${_id}/done`)
-            get().getTask()
-            return res
-        } catch (error) {
-            console.error(error)
-        }
-    },
-    deleteTask: async (_id)=>{
-        try {
-            const res = await api.delete(`/api/tasks/${_id}`)
-            get().getTask()
-            return res
-        } catch (error) {
-            console.error(error)
-        }
-    }
-}))
+    }),
+)
 
 export default taskStore;
--- VocaTask/src/store/userStore.js
+++ VocaTask/src/store/userStore.js
@@ -1,66 +1,86 @@
 import { create } from 'zustand';
+import { persist } from "zustand/middleware";
 import { axiosInstance as api } from '../config/axiosInstance';
 import { getAccessToken, removeAccessToken, saveAccessToken, decodeToken } from '../utils/tokenManager';
 
-const userStore = create((set, get) => ({
+const initialState = {
     user: null,
     userData: null,
+    error: null
+}
 
-    getCurrentUser: () => {
-        const accessToken = getAccessToken();
-        if (accessToken) {
-            set({ user: decodeToken(accessToken)});
+const userStore = create(
+    persist(
+        (set, get) => ({
+            ...initialState,
+
+            // Get Access Token
+            getCurrentUser: () => {
+                const accessToken = getAccessToken();
+                if (accessToken) {
+                    set({ user: decodeToken(accessToken)});
+                }
+            },
+
+            // get Data User
+            getDataUser: async ()=>{
+                try {
+                    const res = await api.get('api/users/profile')
+
+                    set({userData: res.data})
+                } catch (error) {
+                    set({ error: error });
+                    console.error(error)
+                }
+
+            },
+
+            // Login
+            login: async (email, password) => {
+                try {
+                    const res = await api.post('/api/users/login', { email, password });
+                    saveAccessToken(res.data.token);
+                    set({ user: decodeToken(res.data.token) });
+                    return res;
+                } catch (error) {
+                    set({ error: error });
+                    console.error(error);
+                }
+            },
+
+            // Register User
+            register: async (name, email, password) => {
+                try {
+                    const res = await api.post('/api/users/register', { name, email, password });
+
+                    return res;
+                } catch (error) {
+                    console.error(error)
+                }
+            },
+
+            // Update User
+            updateUser: async (data) => {
+                try {
+                    const res = await api.put('/api/users/profile', { ...data});
+                    get().getDataUser()
+                    return res;
+                } catch (error) {
+                    console.error(error)
+                }
+            },
+
+            // Logout User
+            logout: () => {
+                removeAccessToken();
+                set({ user: null, userData: null, error: null });
+            },
+        }),
+        {
+            getStorage: () => localStorage,
+            name: "user-storage",
         }
-    },
-
-    getDataUser: async ()=>{
-        try {
-            const res = await api.get('api/users/profile')
-
-            set({userData: res.data})
-        } catch (error) {
-            console.error(error)
-        }
-
-    },
-
-    login: async (email, password) => {
-        try {
-            const res = await api.post('/api/users/login', { email, password });
-            saveAccessToken(res.data.token);
-            set({ user: decodeToken(res.data.token) });
-            return res;
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    // Register User
-    register: async (name, email, password) => {
-        try {
-            const res = await api.post('/api/users/register', { name, email, password });
-
-            return res;
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    updateUser: async (data) => {
-        try {
-            const res = await api.put('/api/users/profile', { ...data});
-            get().getDataUser()
-            return res;
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    // Logout User
-    logout: () => {
-        removeAccessToken();
-        set({ user: null, userData: null });
-    },
-}));
+    )
+);
 
 export default userStore;
